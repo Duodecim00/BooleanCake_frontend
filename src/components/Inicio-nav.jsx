5333ca5e389ec3b6508a4d246c554a7662af6bae
Thegodlessman <luischang16@hotmail.com>
2024-02-16 18:33:41 -0400

    Creacion del primer componente nav, realizacion de css general del proyecto

---
+++ src/components/Inicio-nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Inicio-nav.css';
+
+function NavLogin(){
+    return (
+        <>
+        
+        <nav className="nav">
+            
+            <span>Inicio</span>
+            <button>Iniciar Sesion</button>
+            <button>Registrarse</button>
+          
+        </nav>
+        
+        </>
+    )
+}
+
+export default NavLogin
