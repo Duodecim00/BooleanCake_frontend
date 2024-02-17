cbe998f16f443e01fd1a8eebffd077f584103e9d
Thegodlessman <luischang16@hotmail.com>
2024-02-17 08:45:05 -0400

    Componente Nav del inicio terminado, comenzando el segundo componente de la pagina de inicio

--- src/components/Inicio-nav.jsx
+++ src/components/Inicio-nav.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import './Inicio-nav.css';
+import styled from 'styled-components';
+
 
 function NavLogin(){
     return (
         <>
-        
-        <nav className="nav">
-            
-            <span>Inicio</span>
-            <button>Iniciar Sesion</button>
-            <button>Registrarse</button>
-          
-        </nav>
-        
+            <NavContainer className="nav-container">
+
+                <a href="#" className="nav-titulo">ButterCream</a>
+                <section className="container">
+                    <p className="nav-btn">Productos</p>
+                    <p className="nav-btn">Nuestra Empresa</p>
+                    <p className="nav-btn">Contactanos</p>
+                </section>
+                <section>
+                    <a href="#" className="nav-btn_login">Iniciar Sesion</a>
+                    <a href="#" className="nav-btn_register">Registrarse</a>
+                </section>
+            </NavContainer>
         </>
     )
 }
 
 export default NavLogin
+
+const NavContainer = styled.nav`
+    
+
+`
