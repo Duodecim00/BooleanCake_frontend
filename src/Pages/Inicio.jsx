08f2c7011023435025596228f07f9dc26a7227b5
Thegodlessman <luischang16@hotmail.com>
2024-02-18 18:43:05 -0400

    Creacion del componente Register(No terminado)

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import NavLogin from '../components/Inicio-Nav/Inicio-nav.jsx'
 import InicioBnv from '../components/Inicio-Bnv/Inicio-bnv.jsx'
-import { BrowserRouter as Router} from 'react-router-dom';
 
 
 function InicioPage(){
---
+++ src/components/Reg-Band/RegBand.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './RegBand.css'
+import { Link } from "react-router-dom";
+
+function RegBand(){
+
+    return(
+        <>
+            
+            <div className="img-container_reg">
+                    <Link to="/" className="band-tittle">Buttercream!</Link>  
+            </div>
+            
+        </>
+    )
+
+}
+
+export default RegBand
