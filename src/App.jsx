33f5fa19701a7f5bd475fbb8f5121701a982c671
Thegodlessman <luischang16@hotmail.com>
2024-02-20 18:11:52 -0400

    Pagina Login terminada, pendiente a hacer la logica

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import InicioPage from './Pages/Inicio/Inicio.jsx';
 import RegPage from './Pages/Register/Register.jsx';
-import Login from './Pages/login/login';
+import LoginPage from './Pages/login/Login.jsx';
 import CakeList from './components/CakeList/CakeList';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<InicioPage/>}/>
         <Route exact path="/registro" element={<RegPage/>}/>
-        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/login" element={<LoginPage/>}/>
         <Route exact path="/products" element={<CakeList/>}/>
         
 
