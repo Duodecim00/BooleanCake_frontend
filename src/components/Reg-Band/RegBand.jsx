6bd6a5a6f47c28c3f2afc0dfa112f07b7f21ff96
CesarP <capr99@hotmail.com>
2024-02-21 21:18:53 -0400

    agregar paquete react-responsive, trabajar el componente reg-band para no mostrarlo en movil

--- src/components/Reg-Band/RegBand.jsx
+++ src/components/Reg-Band/RegBand.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import './RegBand.css'
 import { Link } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 
 function RegBand(){
+    const [isComponentVisible, setComponentVisible] = useState(true);
 
+    const hideComponentOnMobile = useMediaQuery({ maxWidth: 400  }); // Ocultar componente en dispositivos con ancho máximo de 768px
+  // Función para alternar la visibilidad del componente
+  const toggleComponentVisibility = () => {
+    setComponentVisible(!isComponentVisible);
+  };
     return(
         <>
-            
-            <div className="img-container_reg">
+         <div className="img-container_reg">
                     <Link to="/" className="band-tittle">Buttercream!</Link>  
-            </div>
-            
+            </div>   
+            {/* <div> */}
+      {/* {isComponentVisible && ( */}
+        {/* <div> */}
+          {/* Contenido del componente */}
+        {/* </div>
+      )} */}
+
+      {/* {hideComponentOnMobile && (
+        <button onClick={toggleComponentVisibility}>
+          {isComponentVisible ? 'Ocultar' : 'Mostrar'} componente
+        </button>
+{/*  */}
+     {/* {hideComponentOnMobile && (
+        <div className="img-container_reg">
+                    <Link to="/" className="band-tittle">Buttercream!</Link>  
+            </div>  
+      )} 
+            </div> */}
         </>
     )
 
