57ff21bb5d1eea040136e4498a97a8daff574265
Cesar Palacios <56370975+Cesarpalaciosr@users.noreply.github.com>
2024-03-06 03:31:40 -0400

    Update vite.config.js delete comment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-// import dotenv from 'dotenv';
-// // run package config
-// dotenv.config();
+
 import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react';
-// import react from '@vitejs/plugin-react-swc';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
