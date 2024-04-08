b831caf30ff9ea9e68141d0b8ab6c70659d424f3
Duodecim00 <josem2740@gmail.com>
2024-04-08 18:42:45 -0400

    reubicacion de carrusel a inicio.jsx

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,19 +1,18 @@
 // import React from "react";
 import NavLogin from "../../components/navs/UserNav/UserNav.jsx";
 import InicioBnv from '../../components/Inicio-Bnv/Inicio-bnv.jsx'
-
+import CarouselProducts from './components/Carousel/Carousel.jsx';
 
 function InicioPage(){
- 
     return(
         <>
             <div>
                 <NavLogin></NavLogin>
                 <InicioBnv></InicioBnv>
+                <CarouselProducts/>
             </div>
         
         </>
     )
 }
-
 export default InicioPage
