b41c18320024187c4a38f1be1e3e08e565b58c3a
CesarP <capr99@hotmail.com>
2024-04-18 03:33:46 -0400

    orders

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
           <Route exact path="/menu" element={<MenuPage />} />
           <Route
             exact
-            path="/singleproduct"
+            path="/singleproduct/:id"
             element={<PageSingleProduct />}
           ></Route>
         </Routes>
--- src/components/Carousel/ProductCarousel.jsx
+++ src/components/Carousel/ProductCarousel.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Carousel from "react-multi-carousel";
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import "../Carousel/Carousel.css"
 
 export default function ProductCarousel(props){
 
       return(
     
-                    <div className="card_container">
+                    <Link style={{color:'black',textDecoration:'none'}} to={`/singleproduct/${props.id}`} params={{ id: props.id }} className="card_container">
                     <img className="product--image"
                     src={props.url}
                     alt="product image"
@@ -15,7 +16,7 @@ export default function ProductCarousel(props){
                     <h2>{props.name}</h2>
                     <p className="price">${props.price}</p>
 
-                    </div>
+                    </Link>
 
                     
       );
